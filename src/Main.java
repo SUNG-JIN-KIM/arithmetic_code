656429d778f28cc4ddc0162e9d5395ebcd9018e2
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-02 10:16:57 +0900

    addition Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int value = a + b;
+
+        System.out.println(value);
+    }
+}
